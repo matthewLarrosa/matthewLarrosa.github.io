2503f9b95f3a143bf940db82f1a2d332fc871650
Matthew Larrosa <Matthewterra16@gmail.com>
2023-07-08 23:29:54 -0400

    mobile styling for education section top done

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,23 +8,23 @@ function Education() {
       <div className="h-auto w-5/6 md:w-4/6 bg-slate-100 rounded-xl">
         <div className="flex flex-col justify-center space-y-10">
           <div className="flex flex-col justify-center text-center">
-            <div className="font-nav font-medium text-2xl mt-10 ">
+            <div className="font-nav font-medium text-2xl mt-10 md:text-4xl">
               Currently attending
             </div>
-            <div className="font-nav font-bold text-2xl mt-3">
+            <div className="font-nav font-bold text-2xl mt-3 md:text-5xl">
               Florida International University
             </div>
-            <div className="font-nav font-normal text-base mt-3">
+            <div className="font-nav font-normal text-base mt-3 md:text-2xl">
               Junior standing pursuing a Bachelor of Science
             </div>
-            <div className="font-nav font-normal text-base">
+            <div className="font-nav font-normal text-base md:text-2xl">
               in Computer Science.
             </div>
           </div>
           <div className="flex flex-row w-auto justify-center pb-8 space-x-2">
-            <img className="h-28 w-28 p-1" src={init} alt="init" />
-            <img className="h-28 w-28 p-1" src={crest} alt="FIU Crest" />
-            <img className="h-28 w-28 p-1" src={fiuLogo} alt="FIU Knight Logo"/>
+            <img className="h-28 w-28 p-1 md:h-52 md:w-52" src={init} alt="init" />
+            <img className="h-28 w-28 p-1 md:h-52 md:w-52" src={crest} alt="FIU Crest" />
+            <img className="h-28 w-28 p-1 md:h-52 md:w-52" src={fiuLogo} alt="FIU Knight Logo"/>
           </div>
         </div>
       </div>
