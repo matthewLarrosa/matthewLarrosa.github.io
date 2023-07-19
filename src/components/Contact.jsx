e5a211ef8e1f7ebb386749d379ab3ccfd300efde
Matthew Larrosa <Matthewterra16@gmail.com>
2023-07-18 21:02:33 -0400

    email section styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,53 +31,63 @@ function Contact() {
         <div className="font-nav font-bold text-3xl m-6">Contact Me</div>
       </div>
       <div className="flex justify-center mt-2">
-        <div className="flex justify-center h-auto w-5/6 md:w-4/6 bg-slate-100 rounded-xl">
-        <div className="font-nav font-bold text-3xl m-6">Feel free to send me an email! Or you can message me on these platforms.</div>
-          <form ref={form} onSubmit={sendEmail}>
-            <div className="row pt-5 mx-auto">
-              <div className="col-8 form-group mx-auto">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Name"
-                  name="name"
-                />
-              </div>
-              <div className="col-8 form-group mx-auto">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Email Address"
-                  name="email"
-                />
-              </div>
-              <div className="col-8 form-group mx-auto">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Subject"
-                  name="subject"
-                />
-              </div>
-              <div className="col-8 form-group mx-auto">
-                <textarea
-                  className="form-control"
-                  id=""
-                  cols="30"
-                  rows="8"
-                  placeholder="Your message"
-                  name="message"
-                ></textarea>
-              </div>
-              <div className="col-8 form-group mx-auto">
-                <input
-                  type="submit"
-                  className="btn btn-info"
-                  value="Send Message"
-                ></input>
-              </div>
+        <div className="flex justify-center flex-col h-auto w-5/6 md:w-4/6 bg-slate-100 rounded-xl">
+          <div className="flex justify-center text-center flex-col">
+            <div className="font-sec font-bold text-xl m-5">
+            Feel free to send me an email or message me on my socials
             </div>
-          </form>
+          <div className="flex justify-center flex-row">
+            <img>LinkedIn</img>
+            <img>Github</img>
+          </div>
+          </div>
+          <div>
+            <form ref={form} onSubmit={sendEmail}>
+              <div className="row pt-5 mx-auto">
+                <div className="col-8 form-group mx-auto">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                    name="name"
+                  />
+                </div>
+                <div className="col-8 form-group mx-auto">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Email Address"
+                    name="email"
+                  />
+                </div>
+                <div className="col-8 form-group mx-auto">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Subject"
+                    name="subject"
+                  />
+                </div>
+                <div className="col-8 form-group mx-auto">
+                  <textarea
+                    className="form-control"
+                    id=""
+                    cols="30"
+                    rows="8"
+                    placeholder="Your message"
+                    name="message"
+                  ></textarea>
+                </div>
+                <div className="col-8 form-group mx-auto">
+                  <input
+                    type="submit"
+                    className="btn btn-info"
+                    value="Send Message"
+                  ></input>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
