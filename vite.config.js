4d4b573bc60373c933dbeffdd33c6e9437ac4bd5
Matthew Larrosa <Matthewterra16@gmail.com>
2023-08-04 01:21:49 -0400

    hosting

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/matthewlarrosa.github.io'
+  base:'/matthewlarrosa.github.io'
 })
